ea6d091bad57643155a3930199c721da5d0e7150
Calvin Hu <cahu@ucdavis.edu>
2016-08-28 22:25:55 -0700

    First commit

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController: MonoBehaviour
+{
+    public int hitPoints = 2;
+    public Sprite damagedSprite;
+    public float damageImpactSpeed = 5f;
+
+    private int currentHitPoints;
+    private float damageImpactSpeedSqr;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D birdRigidbody2D;
+    private Collider2D birdCollider2D;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        birdRigidbody2D = GetComponent<Rigidbody2D>();
+        birdCollider2D = GetComponent<Collider2D>();
+        currentHitPoints = hitPoints;
+        damageImpactSpeedSqr = damageImpactSpeed * damageImpactSpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Damager")
+            return;
+        if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeedSqr)
+            return;
+
+        spriteRenderer.sprite = damagedSprite;
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        spriteRenderer.enabled = false;
+        birdCollider2D.enabled = false;
+        birdRigidbody2D.isKinematic = true;
+    }
+}
---
+++ Assets/Scripts/ProjectileFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileFollow : MonoBehaviour
+{
+    public Transform projectile;
+    public Transform farLeft;
+    public Transform farRight;
+
+    void Update()
+    {
+        if (projectile != null)
+        {
+            FollowProjectile();
+        }
+    }
+
+    void FollowProjectile()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = projectile.position.x;
+        newPos.x = Mathf.Clamp(newPos.x, farLeft.position.x, farRight.position.x);
+        transform.position = newPos;
+    }
+}
